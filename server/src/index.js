31b8fb862fbd1ad6d120c88572b79ba2a02e0048
Kar <99517165+Karit08@users.noreply.github.com>
2023-04-15 18:26:48 -0600

    SPA initial files

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const data = require("./utils/data");
+
+http.createServer((req, res) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    const {url} = req;
+    if(url.includes("rickandmorty/character/")){
+        const id = Number(url.split("/").at(-1));
+        const character = data.find((char)=> char.id === id);
+        
+        if(character){
+            res.writeHead(200, {"Content-Type": "application/json"});
+            return res.end(JSON.stringify(character));
+        }else{
+            res.writeHead(404, {"Content-Type": "application/json"});
+            return res.end(JSON.stringify({error: "Characyer not found"}));
+        }
+    }
+
+}).listen(3001, "localhost")
+
+//Ruta quiero tener
